6b16bbb8d4183089eb1d68d330f0e4f6e51b613d
Moises Veliz <msveliz16@gmail.com>
2020-04-18 10:03:52 -0400

    se agrego los indicadores de estado, sin tareas, tereas sin terminar y tareas completadas

--- js/toDoList.js
+++ js/toDoList.js
@@ -6,7 +6,8 @@ const btnNewTask = document.querySelector('#btn-new-task'),
 	  headerTitle = document.querySelector('.header__title'),
 	  headerInput = document.querySelector('.header__input');
 	  
-let newTask = '';
+let newTask = '',
+	flatTasksUnfinished = true;
 
 // Evento para cambiar el titulo de mi tarea
 headerTitle.addEventListener('click', ()=>{
@@ -16,17 +17,11 @@ headerTitle.addEventListener('click', ()=>{
 });
 headerInput.children[0].addEventListener('blur',()=>{
 	if( !headerInput.children[0].value ) return;
-	headerTitle.innerText = headerInput.children[0].value;
-	headerInput.classList.add('hide');
-	headerTitle.classList.remove('hide');
+	editTitleList();
 });
 headerInput.children[0].addEventListener('keyup',(event)=>{
 	if( !headerInput.children[0].value ) return;
-	if( event.keyCode == 13 ){
-		headerTitle.innerText = headerInput.children[0].value;
-		headerInput.classList.add('hide');
-		headerTitle.classList.remove('hide');
-	}
+	if( event.keyCode == 13 ) editTitleList();
 });
 
 // Eventos para crear una tarea nueva.
@@ -35,20 +30,26 @@ inputNewTask.addEventListener('keyup', (event) => event.keyCode == 13 &&  !btnNe
 
 // Evento para borrar o editar tarea
 bodyTask.addEventListener('click', event =>{
+	// Esta variable me almacena el contenedoor de las tareas
+	taskParent = event.target.parentNode.parentNode.parentNode;
 
-	task = event.target.parentNode.parentNode.parentNode;
-	taskEdit = task.children[0].children[1].innerText;
+	// Esta Condiional me indica que si no existe un nodo no me ejecutes ese codigo.
+	if( taskParent.children[0].children[1] ){
+		taskEdit = taskParent.children[0].children[1].innerText;
 
-	if( event.target.attributes[0].value === "fas fa-trash-alt" ){
-		bodyTask.removeChild(task);
-		checkTask();
-	}else if ( event.target.attributes[0].value === "far fa-edit" ) {
-		btnEditTask.classList.remove('hide');
-		btnNewTask.classList.add('hide');
-		inputNewTask.value = taskEdit;
-		task.classList.add('editing');
-		activeDisabled();
+		if( event.target.attributes[0].value === "fas fa-trash-alt" ){
+			bodyTask.removeChild(taskParent);
+			checkTask();
+		}else if ( event.target.attributes[0].value === "far fa-edit" ) {
+			btnEditTask.classList.remove('hide');
+			btnNewTask.classList.add('hide');
+			inputNewTask.value = taskEdit;
+			taskParent.classList.add('editing');
+			activeDisabled();
+		}	
 	}
+	flatTasksUnfinished = checkTaskFinish();
+	checkTask();
 });
 
 // Evento para guardar Las tareas editadas.
@@ -65,6 +66,21 @@ inputNewTask.addEventListener('keyup', (event) => {
 
 
 // Funciones-----------------------------
+const checkTaskFinish =()=>{
+	let tasksFinish = 0;
+	let tasksUnfinished = 0;
+	document.querySelectorAll('.falled-in').forEach(checkbox =>{
+		switch ( checkbox.checked ) {
+				case true:
+					tasksFinish++;
+					break;
+				case false:
+					tasksUnfinished++;
+					break;
+			}	
+	});
+	return document.querySelectorAll('.falled-in').length - tasksFinish;
+}
 const activeDisabled =()=>{
 	const btnsArray = bodyTask.querySelectorAll('button');
 	btnsArray.forEach(btn =>{
@@ -78,7 +94,17 @@ const addTask = () => {
 }
 
 // Esta funcion me verifia si tengo tareas, si no tengo tareas me da un mensaje de que no tengo tareas disponibles.
-const checkTask = () => bodyTask.children.length > 1 ? bodyTask.children[0].classList.add('hide') : bodyTask.children[0].classList.remove('hide');
+const checkTask = () =>{
+	if(bodyTask.children.length > 3) bodyTask.children[0].classList.add('hide')
+	else bodyTask.children[0].classList.remove('hide');
+
+	if( bodyTask.children.length < 4 || flatTasksUnfinished === 0) bodyTask.children[1].classList.add('hide');
+	else bodyTask.children[1].classList.remove('hide');
+
+	if( flatTasksUnfinished > 0 || bodyTask.children.length < 4 ) bodyTask.children[2].classList.add('hide');
+	else bodyTask.children[2].classList.remove('hide');
+} 
+checkTask();
 
 const createElement = (element) => {
 	if ( !element ) return;
@@ -86,7 +112,7 @@ const createElement = (element) => {
 	bodyTask.append(newElement);
 	bodyTask.lastChild.classList.add('task');
 	bodyTask.lastChild.innerHTML = `<label>
-										<input type="checkbox" class="falled-in">
+										<input type="checkbox" class="falled-in" value="false">
 										<span>${ element }</span>
 										<button class="btn-delete btn__task btn-floating btn-small waves-effect waves-light right"><i class="fas fa-trash-alt"></i></button>
 										<button class="btn-edit btn__task btn-floating btn-small waves-effect waves-light right"><i class="far fa-edit"></i></button>	
@@ -98,18 +124,18 @@ const createTask = () => {
 	newTask = addTask();
 	createElement( newTask );
 	clearInput();
+	flatTasksUnfinished = true;
 	checkTask();
 }
 
 const clearInput = () => inputNewTask.value = '';
 
 const editTitleList = ()=> {
+	headerTitle.innerText = headerInput.children[0].value;
+	headerInput.classList.add('hide');
+	headerTitle.classList.remove('hide');
+}
 	
-}
-
-const init = () => {
-	checkTask();
-}
 const saveChanges =() => {
 	document.querySelector('.editing').children[0].children[1].innerText = inputNewTask.value;
 	document.querySelector('.editing').classList.remove('editing');
